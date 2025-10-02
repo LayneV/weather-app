608464d175362fae257b829e9a437c8d1ea27c31
LayneV <152360779+LayneV@users.noreply.github.com>
2025-10-01 23:04:32 -0500

    feat(ui) = Display weather location and loading message for location search

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { getData } from "./api.js";
 const searchBtn = document.getElementById("search-button");
 const searchInput = document.getElementById("city-input");
 const weatherTemp = document.getElementById("temp");
+const weatherLocation = document.getElementById("location");
 
 searchBtn.addEventListener("click", async () => {
   const query = searchInput.value.trim();
@@ -11,11 +12,14 @@ searchBtn.addEventListener("click", async () => {
     alert("Please enter a city");
     return null;
   }
-
+  weatherLocation.innerHTML = "Loading...";
   const data = await getData(query);
   if (data === null) {
     weatherTemp.innerHTML = "Type in a correct city.";
+    weatherLocation.innerHTML = "";
   } else {
+    console.log(data);
+    weatherLocation.innerHTML = `${data.resolvedAddress}`;
     weatherTemp.innerHTML = `Current temp: ${Math.round(data.currentConditions.temp)}°F`;
   }
   searchInput.value = "";
